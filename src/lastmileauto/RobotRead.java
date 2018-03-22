082d9afabe1921df28a24eaba6d5f84b2b46f1ef
Elise Lord <eliselord@Elises-MacBook-Air.local>
2018-03-22 08:39:54 +0100

    byter namn

--- src/lastmileauto/RobotRead.java
+++ src/lastmileauto/RobotRead.java
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package paj;
+
+package lastmileauto;
 import java.util.Random;
+import lastmileauto.ControlUI;
+import lastmileauto.DataStore;
 
 public class RobotRead implements Runnable {
     private int sleepTime;
