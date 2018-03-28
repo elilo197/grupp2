d1f89a86a6c5483b6802d1e216eba2891815f17b
Veronika Andrijevic <Veronika@Veronikas-MacBook-Air.local>
2018-03-28 11:03:32 +0200

    La in kod i Uppdrag

--- src/lastmileauto/Uppdrag.java
+++ src/lastmileauto/Uppdrag.java
@@ -5,6 +5,11 @@
  */
 package lastmileauto;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 /**
  *
@@ -14,4 +19,39 @@ public class Uppdrag {
     
     //http://tnk111.n7.se/listaplatser.php
 
+    public static void main(String[] args) {
+        // TODO code application logic here
+     try {
+
+         Uppdrag http = new Uppdrag();
+         String url = " http://tnk111.n7.se/listaplatser.php"; 
+         URL urlobjekt = new URL(url);       
+         HttpURLConnection anslutning = (HttpURLConnection)
+    urlobjekt.openConnection();
+
+         System.out.println("\nAnropar: " + url);
+ 
+         int mottagen_status = anslutning.getResponseCode();
+
+         System.out.println("Statuskod: " + mottagen_status);
+
+ 
+         BufferedReader inkommande = new BufferedReader(new
+
+        InputStreamReader(anslutning.getInputStream()));
+        String inkommande_text;
+        StringBuffer inkommande_samlat = new StringBuffer();
+ 
+        while ((inkommande_text = inkommande.readLine()) != null) {
+                inkommande_samlat.append(inkommande_text);
+        }
+    
+        inkommande.close();
+
+        System.out.println(inkommande_samlat.toString());
+        }
+    
+     catch (Exception e) { System.out.print(e.toString()); }
+
+}
 }
