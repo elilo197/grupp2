ab1aef39d73a056bd31dae4e5ba1116852aceade
helho985 <37623701+helho985@users.noreply.github.com>
2018-04-06 09:41:42 +0200

    Merge branch 'master' of https://github.com/elilo197/grupp2
    # Conflicts:
    #	nbproject/private/private.properties
    #	src/lastmileauto/LastMileAuto.java

--- src/lastmileauto/Uppdrag.java
+++ src/lastmileauto/Uppdrag.java
@@ -17,39 +17,39 @@ public class Uppdrag {
     
     public Uppdrag() {
     
-    System.out.println("Vi är i Uppdrag! Good job!");    
+    //System.out.println("Vi är i Uppdrag! Good job!");    
     //http://tnk111.n7.se/listaplatser.php}
-    // TODO code application logic here
- //    try {
+    
+     try {
 
-//         Uppdrag http = new Uppdrag();
-//         String url = " http://tnk111.n7.se/listaplatser.php"; 
-//         URL urlobjekt = new URL(url);       
-//         HttpURLConnection anslutning = (HttpURLConnection)
-//    urlobjekt.openConnection();
-//
-//         System.out.println("\nAnropar: " + url);
-// 
-//         int mottagen_status = anslutning.getResponseCode();
-//
-//         System.out.println("Statuskod: " + mottagen_status);
-//
-// 
-//         BufferedReader inkommande = new BufferedReader(new
-//
-//        InputStreamReader(anslutning.getInputStream()));
-//        String inkommande_text;
-//        StringBuffer inkommande_samlat = new StringBuffer();
-// 
-//        while ((inkommande_text = inkommande.readLine()) != null) {
-//                inkommande_samlat.append(inkommande_text);
-//        }
-//    
-//        inkommande.close();
-//
-//        System.out.println(inkommande_samlat.toString());
-//        }
-//    
-//     catch (Exception e) { System.out.print(e.toString()); }
+         Uppdrag http = new Uppdrag();
+         String url = " http://tnk111.n7.se/listaplatser.php"; 
+         URL urlobjekt = new URL(url);       
+         HttpURLConnection anslutning = (HttpURLConnection)
+    urlobjekt.openConnection();
+
+         System.out.println("\nAnropar: " + url);
+ 
+         int mottagen_status = anslutning.getResponseCode();
+
+         System.out.println("Statuskod: " + mottagen_status);
+
+ 
+         BufferedReader inkommande = new BufferedReader(new
+
+        InputStreamReader(anslutning.getInputStream()));
+        String inkommande_text;
+        StringBuffer inkommande_samlat = new StringBuffer();
+ 
+        while ((inkommande_text = inkommande.readLine()) != null) {
+                inkommande_samlat.append(inkommande_text);
+        }
+    
+        inkommande.close();
+
+        System.out.println(inkommande_samlat.toString());
+        }
+    
+     catch (Exception e) { System.out.print(e.toString()); }
 }
 }
