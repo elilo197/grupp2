c67538cb79933094ce93f03931904f139e2f4041
Elise Lord <eliselord@Elises-MacBook-Air.local>
2018-04-16 12:01:00 +0200

    elise

--- src/lastmileauto/RobotRead.java
+++ src/lastmileauto/RobotRead.java
@@ -6,7 +6,6 @@ package lastmileauto;
 import java.util.*;
 
 public class RobotRead implements Runnable  {
-   private int xy;
     private int sleepTime;
     private static Random generator = new Random();
     private ControlUI cui;
@@ -16,6 +15,7 @@ public class RobotRead implements Runnable  {
     private BluetoothTransceiver btc;
     long start;
     long timemilli = 0;
+    int mottagenInt;
 
     public RobotRead(DataStore ds, ControlUI cui, BluetoothReceiver bre) {
         this.cui = cui;
@@ -27,21 +27,22 @@ public class RobotRead implements Runnable  {
 @Override
 public void run () {
 // while (anslutning == true) {// timenano = System.nanoTime();
-   start = System.currentTimeMillis();
+    mottagenInt = Integer.parseInt(bre.mottaget); //Gör om deras string till en int innehåll nodnummer.
+
+   start = System.currentTimeMillis(); //start tid 
    System.out.println("Starttid: " + System.currentTimeMillis());
    
    while (System.currentTimeMillis() - start < 2500){
       
         System.out.println("Tiden i millisekunder är: " + (System.currentTimeMillis() - start));
-//        if (bre.mottaget == XY){
-       //  xy=bre.mottaget; 
-           gui = new GuiUpdate(ds, cui, xy);
 
-//        }
 
-//        if (bre.mottaget == 1){
-//           start = System.currentTimeMillis();
-//        }
+        if (bre.mottaget =="ok"){
+          start = System.currentTimeMillis();
+      }
+        else{
+           gui = new GuiUpdate(ds, cui, mottagenInt);
+       }
 //    
    }
    cui.appendStatus("Nu har det gått för lång tid."); 
