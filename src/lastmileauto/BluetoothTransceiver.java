7d37a923a617075464500fce3fe53e42c6b15542
helho985 <37623701+helho985@users.noreply.github.com>
2018-04-24 14:54:21 +0200

    Merge branch 'master' of https://github.com/elilo197/grupp2
    # Conflicts:
    #	nbproject/private/private.properties
    #	src/lastmileauto/LastMileAuto.java

--- src/lastmileauto/BluetoothTransceiver.java
+++ src/lastmileauto/BluetoothTransceiver.java
@@ -1,32 +1,28 @@
 /*
-anslutning = en streamconnenction
-bluetooth_ut = en printstram som använder anslutning på nått sätt
+
  */
 package lastmileauto;
 
 //Bluetoothnamn flr Arduino 20:16:01:20:56:82
 import java.io.*;
 import javax.microedition.io.*;
-import javax.bluetooth.*;
 
 public class BluetoothTransceiver{
-    PrintStream bluetoothanslutning;
-    StreamConnection anslutning;
-    DataStore ds;
-    ControlUI cui;
+    String meddelande_in;
+   // ControlUI cui;
 
     public BluetoothTransceiver() {
         
-        System.out.println("Nu är vi i tranciver");
-        
-         try {
-             anslutning = (StreamConnection)
-                     Connector.open("btspp://201601205682:1");
-             bluetoothanslutning = new PrintStream(anslutning.openDataOutputStream());
-             cui.appendStatus("Bluetoothanslutning upprättad");
+       try {
+             StreamConnection anslutning = (StreamConnection)
+             Connector.open("btspp://201601205682:1");
+             BufferedReader bluetooth_in = new BufferedReader(new InputStreamReader(anslutning.openInputStream()));
+             //cui.appendStatus("Bluetoothanslutning upprättad");
          
        } catch (Exception e) {  System.out.print(e.toString());   }
-        
-   
+    }
+    
+    public String getMessage(){
+        return meddelande_in;
     }
     }
