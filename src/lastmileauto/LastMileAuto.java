b6bb09653df79a54266fc68fc1a779b431c8282c
Hanna Mellqvist <hannamellqvist@Hannas-MacBook-Air-2.local>
2018-04-24 10:40:49 +0200

    hanna

--- src/lastmileauto/LastMileAuto.java
+++ src/lastmileauto/LastMileAuto.java
@@ -22,9 +22,9 @@ public class LastMileAuto {
          */
         
        //ds.setFileName("C:\\Users\\Helena\\Documents\\GitHub\\grupp2/streets.txt");
-     ds.setFileName("/Users/eliselord/Documents/grupp2/streets.txt");
+     //ds.setFileName("/Users/eliselord/Documents/grupp2/streets.txt");
       //ds.setFileName("/Users/Veronika/Desktop/streets.txt");
-     //ds.setFileName("/Users/hannamellqvist/Documents/Kandidat/grupp2/streets.txt");
+     ds.setFileName("/Users/hannamellqvist/Documents/Kandidat/grupp2/streets.txt");
        //ds.setFileName("/Users/aliceneu/Documents/grupp2/streets.txt");
        
         ds.readNet();
