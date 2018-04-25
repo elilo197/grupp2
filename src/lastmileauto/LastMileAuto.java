2a1cf7245c1d26af8654c8e081f3045bfab1c92a
Elise Lord <eliselord@Elises-MacBook-Air.local>
2018-04-25 08:53:59 +0200

    elise

--- src/lastmileauto/LastMileAuto.java
+++ src/lastmileauto/LastMileAuto.java
@@ -18,12 +18,9 @@ public class LastMileAuto {
 
         /*
          * This sets the file path and read network text file. Adjust for your needs.
-         */
-        
+         */   
 
-
-
-       ds.setFileName("C:\\Users\\Helena\\Documents\\GitHub\\grupp2/streets.txt");
+       //ds.setFileName("C:\\Users\\Helena\\Documents\\GitHub\\grupp2/streets.txt");
        //ds.setFileName("/Users/eliselord/Documents/grupp2/streets.txt");
       // ds.setFileName("/Users/Veronika/Desktop/streets.txt");
       // ds.setFileName("C:\\Users\\Helena\\Documents\\GitHub\\grupp2/streets.txt");
@@ -39,7 +36,7 @@ public class LastMileAuto {
        //ds.setFileName("/Users/Veronika/Desktop/streets.txt");
        //ds.setFileName("/Users/Veronika/Desktop/streets_gammal.txt");
        //ds.setFileName("/Users/hannamellqvist/Documents/Kandidat/grupp2/streets.txt");
-       ds.setFileName("/Users/hannamellqvist/Documents/Kandidat/grupp2/streets.txt");
+      // ds.setFileName("/Users/hannamellqvist/Documents/Kandidat/grupp2/streets.txt");
        //ds.setFileName("/Users/aliceneu/Documents/grupp2/streets.txt");
 
         ds.readNet();
