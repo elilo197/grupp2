3e224a335076bfd82d5f0d894f8537e365532cf1
Elise Lord <eliselord@Elises-MacBook-Air.local>
2018-05-07 09:29:48 +0200

    räknar poäng

--- src/lastmileauto/LastMileAuto.java
+++ src/lastmileauto/LastMileAuto.java
@@ -30,7 +30,7 @@ public class LastMileAuto {
 
        //ds.setFileName("/Users/eliselord/Documents/grupp2/streets.txt");
        //ds.setFileName("C:\\Users\\Helena\\Documents\\GitHub\\grupp2/streets.txt");
-       //ds.setFileName("/Users/eliselord/Documents/grupp2/streets.txt");
+       ds.setFileName("/Users/eliselord/Documents/grupp2/streets.txt");
        //ds.setFileName("/Users/Veronika/Desktop/streets.txt");
 
         //ds.setFileName("/Users/hannamellqvist/Documents/Kandidat/grupp2/streets.txt");
@@ -38,7 +38,7 @@ public class LastMileAuto {
        // ds.setFileName("/Users/hannamellqvist/Documents/Kandidat/grupp2/streets.txt");
        // ds.setFileName("/Users/hannamellqvist/Documents/Kandidat/grupp2/streets.txt");
 
-       ds.setFileName("/Users/aliceneu/Documents/grupp2/streets.txt");
+       //ds.setFileName("/Users/aliceneu/Documents/grupp2/streets.txt");
      //ds.setFileName("/Users/eliselord/Documents/grupp2/streets.txt");
      //ds.setFileName("C:\\Users\\Helena\\Documents\\GitHub\\grupp2/streets.txt");
      //ds.setFileName("/Users/Veronika/Desktop/streets.txt");
